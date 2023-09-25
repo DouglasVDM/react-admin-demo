bca71cf0566316ae8610164dd26a37f41d9a2004
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-09-25 12:45:22 +0200

    Display a list of users by applying ListGuesser

--- admin-demo/src/App.js
+++ admin-demo/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Admin} from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import React from "react";
+import { Admin, ListGuesser, Resource } from "react-admin";
+import restProvider from "ra-data-simple-rest";
 
-const dataProvider = restProvider('http://localhost:3000');
+const dataProvider = restProvider("http://localhost:3000");
 
 function App() {
   return (
-      <Admin dataProvider={dataProvider} />
-    );
-  }
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  );
+}
 export default App;
