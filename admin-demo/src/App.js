5028607d72ce5f8d2d875387e340bf1b9d81d06c
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-09-25 20:44:25 +0200

    Import UserCreate component

--- admin-demo/src/App.js
+++ admin-demo/src/App.js
@@ -3,14 +3,19 @@ import { Admin, Resource } from "react-admin";
 import restProvider from "ra-data-simple-rest";
 
 // Components
-import { UserEdit, UserList } from "./components/User";
+import { UserCreate, UserEdit, UserList } from "./components/User";
 
 const dataProvider = restProvider("http://localhost:3000");
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} edit={UserEdit} />
+      <Resource
+        name="users"
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
+      />
     </Admin>
   );
 }
