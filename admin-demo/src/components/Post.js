06ffb5bd00e52a9a1c17097439bfa3654cad0342
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-09-25 21:12:08 +0200

    Create Post component

---
+++ admin-demo/src/components/Post.js
@@ -0,0 +1,51 @@
+import {
+  List,
+  Create,
+  Edit,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  Datagrid,
+  TextField,
+  DateField,
+  NumberInput,
+} from "react-admin";
+
+export const PostList = () => {
+  return (
+    <List>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="title" />
+        <DateField source="publishedAt" />
+        <TextField source="userId" label="Author" />
+      </Datagrid>
+    </List>
+  );
+};
+
+export const PostCreate = () => {
+  return (
+    <Create title="Create a Post">
+      <SimpleForm>
+        <NumberInput source="id" />
+        <TextInput source="title" />
+        <TextInput multiline source="body" />
+        <DateInput label="Published" source="publishedAt" />
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export const PostEdit = () => {
+  return (
+    <Edit>
+      <SimpleForm>
+        <NumberInput disabled source="id" />
+        <TextInput source="title" />
+        <TextInput multiline source="body" />
+        <DateInput label="Published" source="publishedAt" />
+      </SimpleForm>
+    </Edit>
+  );
+};
