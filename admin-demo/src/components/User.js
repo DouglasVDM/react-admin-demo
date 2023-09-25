a8c9ca296ae1dd8eaaeb522956e2fb6ee0a9264a
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-09-25 22:01:02 +0200

    Theming - MUI style with sx prop CSS-in-JS

--- admin-demo/src/components/User.js
+++ admin-demo/src/components/User.js
@@ -12,7 +12,15 @@ import { Edit, NumberInput, SimpleForm, TextInput } from "react-admin";
 export const UserList = () => {
   return (
     <List>
-      <Datagrid rowClick="edit">
+      <Datagrid rowClick="edit"
+        sx={{
+            ".RaDatagrid-rowEven": {
+              backgroundColor: "lavender",
+            },
+            ".RaDatagrid-headerCell": {
+              backgroundColor: "MistyRose",
+            },
+          }}>
         <TextField source="id" />
         <TextField source="name" />
         <TextField source="username" />
