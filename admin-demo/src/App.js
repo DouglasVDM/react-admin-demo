7ba55259e902a230b441c0a736e7ef5a175cab5d
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-09-25 22:21:05 +0200

    Added unique icons to users and posts

--- admin-demo/src/App.js
+++ admin-demo/src/App.js
@@ -4,6 +4,9 @@ import restProvider from "ra-data-simple-rest";
 import { UserCreate, UserEdit, UserList } from "./components/User";
 import { PostCreate, PostEdit, PostList } from "./components/Post";
 import { authProvider } from "./authProvider";
+import PeopleIcon from "@mui/icons-material/People";
+import ArticleIcon from "@mui/icons-material/Article";
+
 
 const dataProvider = restProvider("http://localhost:3000");
 
@@ -16,12 +19,14 @@ function App() {
         create={UserCreate}
         edit={UserEdit}
         recordRepresentation={(user)=>user.username}
+        icon={PeopleIcon}
       />
       <Resource
         name="posts"
         list={PostList}
         create={PostCreate}
         edit={PostEdit}
+        icon={ArticleIcon}
       />
     </Admin>
   );
